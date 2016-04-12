d3b27e697d405dc216b4b5d2a5c88962084a311c
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-04-12 21:28:26 +0200

    Added some assets and modified some that had backgrounds... Trying to solve a problem with loading the new assets. Loading page working.

--- js/preloader.js
+++ js/preloader.js
@@ -15,15 +15,33 @@ HelloPhaser.Preload.prototype = {
         //Here we show it
         this.load.setPreloadSprite(this.preloadBar);
 
-        //Loading background/map assets
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
+        //Loading assets and tiles for the map
+        this.load.tilemap('lvl1', 'assets/mapa.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.image('Background', 'assets/Tiles/Background.png');
+        this.load.image('mapsheet', 'assets/Tiles/_Spritesheet.png');
+        this.load.image('Tile1', 'assets/Tiles/Tile_1.png');
+        this.load.image('Tile2', 'assets/Tiles/Tile_2.png');
+        this.load.image('Tile3', 'assets/Tiles/Tile_3.png');
+        this.load.image('Tile4', 'assets/Tiles/Tile_4.png');
+        this.load.image('Tile5', 'assets/Tiles/Tile_5.png');
+        this.load.image('Tile6', 'assets/Tiles/Tile_6.png');
+        this.load.image('Tile7', 'assets/Tiles/Tile_7.png');
+        this.load.image('Tile8', 'assets/Tiles/Tile_8.png');
+        this.load.image('Tile9', 'assets/Tiles/Tile_9.png');
+        this.load.image('Tile10', 'assets/Tiles/Tile_10.png');
+        this.load.image('Tile11', 'assets/Tiles/Tile_11.png');
+        this.load.image('Tile12', 'assets/Tiles/Tile_12.png');
+        this.load.image('Tile13', 'assets/Tiles/Tile_13.png');
+        this.load.image('Tile14', 'assets/Tiles/Tile_14.png');
+        this.load.image('Tile15', 'assets/Tiles/Tile_15.png');
+
+        //Loading player
+        this.load.image('static1', 'assets/Character/state1.png');
+        this.load.image('static2', 'assets/Charater/state2.png');
 
         //Loading items
         this.load.image('star', 'assets/star.png');
 
-        //loading sprites
-        this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     },
 
     create: function() {
